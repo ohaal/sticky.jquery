d420ce3d1593a0d560be419c87312f6d0a4879dd
Walmik Deshpande <walmik.deshpande@gmail.com>
2013-04-08 20:11:27 -0700

    added support for tables with or without thead element

--- sticky.jquery.js
+++ sticky.jquery.js
@@ -18,6 +18,9 @@
     $.fn.sticky = function()
     {
         var tableId = this;
+
+        //set border collapse
+        //$(tableId).css("border-collapse", "collapse");
         //collect widths of all the <th> elements
         var thWidthsArr = [];
         $(tableId).find("th").each(function(){
@@ -27,29 +30,36 @@
         //set the distance of the table from the top,
         //we ll need to make the headers sticky when this distance is 0  
         var thTop = pos.top + "px";
+
+        //identify first table row
+        var firstRow = "tr:first-child";
+        //change firstRow in case there is a thead tag used
+        var hasTHead = $(tableId).find("thead").length;    
+        if(hasTHead) firstRow = "thead tr:first-child";
+
         //set the widths of the first and last tr's ths/tds...
         //this is done coz in some cases,
         //the widths will get messed up if the data was generated dynamically
         var count = 0;
-        $(tableId).find("tr:first-child>th").each(function(){
+        $(tableId).find(firstRow + ">th").each(function(){
             $(this).css("width", thWidthsArr[count]);
             count++;
         });
         count = 0;
-        $(tableId).find("tr:last-child>td").each(function(){
+        $(tableId).find(firstRow + ">td").each(function(){
             $(this).css("width", thWidthsArr[count]);
             count++;
         });
         $(window).scroll(function(){
             if($(window).scrollTop() > pos.top)
             {
-                $(tableId).find("thead tr:first-child").css("position", "fixed");
-                $(tableId).find("thead tr:first-child").css("top", "0px");
+                $(tableId).find(firstRow).css("position", "fixed");
+                $(tableId).find(firstRow).css("top", "0px");
             }
             else
             {
-                $(tableId).find("thead tr:first-child").css("position", "relative");
-                $(tableId).find("thead tr:first-child").css("top", thTop);
+                $(tableId).find(firstRow).css("position", "relative");
+                $(tableId).find(firstRow).css("top", thTop);
             }
         });
         
