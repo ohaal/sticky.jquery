01a00a8e21f34e49a67fab27a95a7a83a58ec439
Walmik <walmik.deshpande@gmail.com>
2013-05-16 10:17:34 -0700

    enabled support for broad tables that required horizontal scrolling

--- sticky.jquery.js
+++ sticky.jquery.js
@@ -22,6 +22,7 @@
         //set border collapse
         //$(tableId).css("border-collapse", "collapse");
         //collect widths of all the <th> elements
+        var tableWidth = $(tableId).width();
         var thWidthsArr = [];
         $(tableId).find("th").each(function(){
             thWidthsArr.push($(this).css("width"));
@@ -30,9 +31,11 @@
         //set the distance of the table from the top,
         //we ll need to make the headers sticky when this distance is 0  
         var thTop = pos.top + "px";
+        var thLeft = pos.left;
 
         //identify first table row
         var firstRow = "tr:first-child";
+
         //change firstRow in case there is a thead tag used
         var hasTHead = $(tableId).find("thead").length;    
         if(hasTHead) firstRow = "thead tr:first-child";
@@ -46,15 +49,25 @@
             count++;
         });
         count = 0;
-        $(tableId).find(firstRow + ">td").each(function(){
+
+        //assign the widths to td elements of a tr row
+        //choose the last tr coz the first tr has th elements
+        var lastRow = "tr:last-child";
+        $(tableId).find(lastRow + ">td").each(function(){
             $(this).css("width", thWidthsArr[count]);
             count++;
         });
+
+
         $(window).scroll(function(){
             if($(window).scrollTop() > pos.top)
             {
-                $(tableId).find(firstRow).css("position", "fixed");
-                $(tableId).find(firstRow).css("top", "0px");
+                $(tableId).find(firstRow).css("width", tableWidth+"px");
+                $(tableId).find(lastRow).css("width", tableWidth+"px");
+                $(tableId).find(firstRow).css("position", "absolute");
+                $(tableId).find(firstRow).css("top", $("body")[0].scrollTop + "px");
+                $(tableId).find(firstRow).css("left", thLeft+ "px");
+                console.log($(tableId).scrollTop());
             }
             else
             {
